8433fe7952ea0dca3e09f03bed2ba6c28c16c796
antonreche <antonreche@gmail.com>
2015-05-09 20:50:47 +0200

    testSonIguales

--- src/unit9/CadenasAlumnoTest.java
+++ src/unit9/CadenasAlumnoTest.java
@@ -10,12 +10,15 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import junit.framework.TestCase;
 
+import junit.framework.*;
 /**
  * @author Antonio Reche Ramal
  */
 public class CadenasAlumnoTest {
 
+	private CadenasAlumno cad;
 	/**
 	 * @throws java.lang.Exception
 	 */
@@ -34,14 +37,15 @@ public class CadenasAlumnoTest {
 	 * @throws java.lang.Exception
 	 */
 	@Before
-	public void setUp() throws Exception {
+	public void setUp(){
+		cad=new CadenasAlumno();		
 	}
 
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@After
-	public void tearDown() throws Exception {
+	public void tearDown(){		
 	}
 
 	/**
@@ -49,12 +53,18 @@ public class CadenasAlumnoTest {
 	 */
 	@Test
 	public void testSonIguales() {
-		fail("Not yet implemented");
-	}
+		
+		String cad1="entornos";
+		String cad2="entoroooo";
+		String cad3="entornos";
+		assertEquals(cad.sonIguales(cad1,cad3),cad1.equals(cad3));
+		assertEquals(!cad.sonIguales(cad1,cad2),!cad1.equals(cad2));
+		assertTrue(cad.sonIguales(cad1, cad3) && cad1.equalsIgnoreCase(cad3));
+	}	
 
 	/**
 	 * Test method for {@link unit9.CadenasAlumno#esMayor(java.lang.String, java.lang.String)}.
-	 */
+	 *
 	@Test
 	public void testEsMayor() {
 		fail("Not yet implemented");
@@ -62,7 +72,7 @@ public class CadenasAlumnoTest {
 
 	/**
 	 * Test method for {@link unit9.CadenasAlumno#esMenor(java.lang.String, java.lang.String)}.
-	 */
+	 *
 	@Test
 	public void testEsMenor() {
 		fail("Not yet implemented");
@@ -70,7 +80,7 @@ public class CadenasAlumnoTest {
 
 	/**
 	 * Test method for {@link unit9.CadenasAlumno#comparaIgnorandoMayusculas(java.lang.String, java.lang.String)}.
-	 */
+	 *
 	@Test
 	public void testComparaIgnorandoMayusculas() {
 		fail("Not yet implemented");
@@ -78,7 +88,7 @@ public class CadenasAlumnoTest {
 
 	/**
 	 * Test method for {@link unit9.CadenasAlumno#reemplazaTodos(java.lang.String, java.lang.String, java.lang.String)}.
-	 */
+	 *
 	@Test
 	public void testReemplazaTodos() {
 		fail("Not yet implemented");
@@ -86,7 +96,7 @@ public class CadenasAlumnoTest {
 
 	/**
 	 * Test method for {@link unit9.CadenasAlumno#reemplazaPrimero(java.lang.String, java.lang.String, java.lang.String)}.
-	 */
+	 *
 	@Test
 	public void testReemplazaPrimero() {
 		fail("Not yet implemented");
@@ -94,7 +104,7 @@ public class CadenasAlumnoTest {
 
 	/**
 	 * Test method for {@link unit9.CadenasAlumno#muestraSplitLimite(java.lang.String, java.lang.String, int)}.
-	 */
+	 *
 	@Test
 	public void testMuestraSplitLimite() {
 		fail("Not yet implemented");
@@ -102,7 +112,7 @@ public class CadenasAlumnoTest {
 
 	/**
 	 * Test method for {@link unit9.CadenasAlumno#muestraSplit(java.lang.String, java.lang.String)}.
-	 */
+	 *
 	@Test
 	public void testMuestraSplit() {
 		fail("Not yet implemented");
@@ -110,7 +120,7 @@ public class CadenasAlumnoTest {
 
 	/**
 	 * Test method for {@link unit9.CadenasAlumno#quitaEspacios(java.lang.String)}.
-	 */
+	 *
 	@Test
 	public void testQuitaEspacios() {
 		fail("Not yet implemented");
@@ -118,7 +128,7 @@ public class CadenasAlumnoTest {
 
 	/**
 	 * Test method for {@link unit9.CadenasAlumno#convertirMayusculas(java.lang.String)}.
-	 */
+	 *
 	@Test
 	public void testConvertirMayusculas() {
 		fail("Not yet implemented");
@@ -126,7 +136,7 @@ public class CadenasAlumnoTest {
 
 	/**
 	 * Test method for {@link unit9.CadenasAlumno#convertirMinusculas(java.lang.String)}.
-	 */
+	 *
 	@Test
 	public void testConvertirMinusculas() {
 		fail("Not yet implemented");
@@ -134,7 +144,7 @@ public class CadenasAlumnoTest {
 
 	/**
 	 * Test method for {@link unit9.CadenasAlumno#longitudCadena(java.lang.String)}.
-	 */
+	 *
 	@Test
 	public void testLongitudCadena() {
 		fail("Not yet implemented");
@@ -142,7 +152,7 @@ public class CadenasAlumnoTest {
 
 	/**
 	 * Test method for {@link unit9.CadenasAlumno#empiezaCon(java.lang.String, java.lang.String)}.
-	 */
+	 *
 	@Test
 	public void testEmpiezaCon() {
 		fail("Not yet implemented");
@@ -150,7 +160,7 @@ public class CadenasAlumnoTest {
 
 	/**
 	 * Test method for {@link unit9.CadenasAlumno#acabaEn(java.lang.String, java.lang.String)}.
-	 */
+	 *
 	@Test
 	public void testAcabaEn() {
 		fail("Not yet implemented");
@@ -158,7 +168,7 @@ public class CadenasAlumnoTest {
 
 	/**
 	 * Test method for {@link unit9.CadenasAlumno#posicionPrimeraCadena(java.lang.String, java.lang.String)}.
-	 */
+	 *
 	@Test
 	public void testPosicionPrimeraCadena() {
 		fail("Not yet implemented");
@@ -166,7 +176,7 @@ public class CadenasAlumnoTest {
 
 	/**
 	 * Test method for {@link unit9.CadenasAlumno#extraerSubstring(java.lang.String, int)}.
-	 */
+	 *
 	@Test
 	public void testExtraerSubstringStringInt() {
 		fail("Not yet implemented");
@@ -174,7 +184,7 @@ public class CadenasAlumnoTest {
 
 	/**
 	 * Test method for {@link unit9.CadenasAlumno#extraerSubstring(java.lang.String, int, int)}.
-	 */
+	 *
 	@Test
 	public void testExtraerSubstringStringIntInt() {
 		fail("Not yet implemented");
@@ -182,7 +192,7 @@ public class CadenasAlumnoTest {
 
 	/**
 	 * Test method for {@link unit9.CadenasAlumno#concatenaCadenas(java.lang.String, java.lang.String)}.
-	 */
+	 *
 	@Test
 	public void testConcatenaCadenas() {
 		fail("Not yet implemented");
@@ -190,10 +200,10 @@ public class CadenasAlumnoTest {
 
 	/**
 	 * Test method for {@link unit9.CadenasAlumno#main(java.lang.String[])}.
-	 */
+	 *
 	@Test
 	public void testMain() {
 		fail("Not yet implemented");
 	}
-
+*/
 }
